40f1f0cf218d1f7e866b890228fb3a942469e422
Morgan Hewitt <morganhewitt0211@gmail.com>
2026-01-29 19:23:20 +0100

    Creating the JSON dataset! Needs to be checked but looks good!

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 from dotenv import load_dotenv
 import os
 import wikirate4py
+import json
 
 load_dotenv()
 api_key = os.getenv("WIKIRATE_API_KEY")
 api = wikirate4py.API(api_key)
 
-# my chosen metrics 
-# 1 - Total energy consumption
-# 2 - Direct greenhouse gas emissions
-# 3 - Incidents of discrimination
+metrics = [
+    846580, # total energy consumption
+    826615, # direct greenhouse gas emissions (scope 1)
+    836314 # incidents of discriimination
+]
 
-def get_companies_all_metrics(identifier1, identifier2, identifier3, limit):
+def get_company_ids__in_all_metrics(metrics, limit):
     metric_one_company_names = []
     metric_two_company_names = []
     metric_three_company_names = []
 
-    metric_one_company_data = api.get_answers(identifier=identifier1, limit=limit, status="known")
-    metric_two_company_data = api.get_answers(identifier=identifier2, limit=limit, status="known")
-    metric_three_company_data = api.get_answers(identifier=identifier3, limit=limit, status="known")
+    metric_one_company_data = api.get_answers(identifier=metrics[0], limit=limit, status="known")
+    metric_two_company_data = api.get_answers(identifier=metrics[1], limit=limit, status="known")
+    metric_three_company_data = api.get_answers(identifier=metrics[2], limit=limit, status="known")
 
 
     for metric_one_company in metric_one_company_data:
         metric_one_company_names.append(metric_one_company.company)
+        
 
     for metric_two_company in metric_two_company_data:
         metric_two_company_names.append(metric_two_company.company)
@@ -30,63 +33,70 @@ def get_companies_all_metrics(identifier1, identifier2, identifier3, limit):
     for metric_three_company in metric_three_company_data:
         metric_three_company_names.append(metric_three_company.company)
 
-    company_IDs_in_all_metrics = list(set(metric_one_company_names) & set(metric_two_company_names) & set(metric_three_company_names))
+    companies_in_all_metrics = list(set(metric_one_company_names) & set(metric_two_company_names) & set(metric_three_company_names))
           
-    return company_IDs_in_all_metrics[:10]
+    companies_in_all_metrics = companies_in_all_metrics[:10]
 
-all_companies = get_companies_all_metrics(846580, 826615, 836314, 100)
-#print(all_companies)
-#print(len(all_companies))
+    company_ids = []
 
-# company and metric identifiers
-company_id = 5760335 # equinor
-metric_id = 846580 #total energy consumption
+    for company in companies_in_all_metrics:
+        id = api.get_company(company).id
+        company_ids.append(id)
 
-# fetching metric info, company info, and all answers
-metric = api.get_metric(identifier=metric_id)
-company = api.get_company(company_id)
-answers = api.get_answers(identifier=metric_id, company=company_id, limit=100, status="known")
+    return company_ids
+
+company_ids_list = get_company_ids__in_all_metrics(metrics, 100)
 
 dataset = []
 
-for answer in answers:
+for metric_id in metrics:
+    for company_id in company_ids_list:
 
-    source_docs = [] 
+        # fetching metric info, company info, and all answers
+        metric = api.get_metric(identifier=metric_id)
+        company = api.get_company(company_id)
 
-    sources = answer.sources
-    for source_id in sources:
-        try:
-            source = api.get_source(identifier=source_id)
-            source_docs.append({
-                "file_name": source.title,
-                "file_url": source.file_url,
-            })
-            
-        except wikirate4py.exceptions.NotFoundException:
-            source_docs.append({
-                "file_name": "None",
-                "file_url": "None",
-                "source_id": source_id
-            })
+        answers = api.get_answers(identifier=metric_id, company=company_id, limit=100, status="known")
 
-    record = {
-        "input": {
-            "question": metric.question,
-            "custom_id": metric.name,
-            "data_type": metric.value_type,
-            "company": company.name,
-            "file_metas:": source_docs
-        },
-        "reference_output": {
-            "answer": answer.value,
-            "source_documents": source_docs
-        },
-        "structured_data": {
-            "time_period": answer.year
-        }
-    }
+        for answer in answers:
 
-    dataset.append(record)
+            source_docs = [] 
 
-print(len(dataset))
+            sources = answer.sources
+            for source_id in sources:
+                try:
+                    source = api.get_source(identifier=source_id)
+                    source_docs.append({
+                        "file_name": source.title,
+                        "file_url": source.file_url,
+                    })
+                    
+                except wikirate4py.exceptions.NotFoundException:
+                    source_docs.append({
+                        "file_name": "None",
+                        "file_url": "None",
+                        "source_id": source_id
+                    })
 
+            record = {
+                "input": {
+                    "question": metric.question,
+                    "custom_id": metric.name,
+                    "data_type": metric.value_type,
+                    "company": company.name,
+                    "file_metas": source_docs
+                },
+                "reference_output": {
+                    "answer": answer.value,
+                    "source_documents": source_docs
+                },
+                "structured_data": {
+                    "time_period": answer.year
+                }
+            }
+
+            dataset.append(record)
+
+
+with open("dataset.json", "w", encoding="utf-8") as f:
+    json.dump(dataset, f, indent=2, ensure_ascii=False)
